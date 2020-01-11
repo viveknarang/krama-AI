c5333fe3b0ca79133073d688a0e5476b16b4311c
Vivek Narang <vivek.narang10@gmail.com>
2020-01-11 00:47:57 -0500

    refactoring ...

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
 const properyFile = "/home/narang/work/src/github.com/viveknarang/kramaAPI/api.properties"
@@ -16,15 +14,7 @@ func main() {
 	connectDB(MongoURL, MongoPort)
 	connectRedis(RedisURL, RedisPort)
 
-	r := mux.NewRouter()
-
-	r.HandleFunc("/customers/"+APIVersion+"/login", login).Methods(http.MethodPost)
-	r.HandleFunc(CatalogPath+"/products/{SKU}", getProduct).Methods(http.MethodGet)
-	r.HandleFunc(CatalogPath+"/products", postProduct).Methods(http.MethodPost)
-	r.HandleFunc(CatalogPath+"/products/{SKU}", putProduct).Methods(http.MethodPut)
-	r.HandleFunc(CatalogPath+"/products/{SKU}", deleteProduct).Methods(http.MethodDelete)
-
-	log.Fatal(http.ListenAndServe(":"+APIPort, r))
+	log.Fatal(http.ListenAndServe(":"+APIPort, routers()))
 
 	disconnectDB()
 	disconnectRedis()
---
+++ router.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func routers() *mux.Router {
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/customers/"+APIVersion+"/login", login).Methods(http.MethodPost)
+
+	router.HandleFunc(CatalogPath+"/products/{SKU}", getProduct).Methods(http.MethodGet)
+
+	router.HandleFunc(CatalogPath+"/products", postProduct).Methods(http.MethodPost)
+
+	router.HandleFunc(CatalogPath+"/products/{SKU}", putProduct).Methods(http.MethodPut)
+
+	router.HandleFunc(CatalogPath+"/products/{SKU}", deleteProduct).Methods(http.MethodDelete)
+
+	return router
+
+}
