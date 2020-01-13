2e73d1ef7b88387790179b5ae37247c9f2bedc72
Vivek Narang <vivek.narang10@gmail.com>
2020-01-13 16:52:50 -0500

    refactoring core redis and mongo ...

--- core.mongo.go
+++ core.mongo.go
@@ -10,10 +10,10 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-//CLIENT mongo client
-var CLIENT *mongo.Client
+//MONGODBCLIENT mongo client
+var MONGODBCLIENT *mongo.Client
 
-func connectDB() *mongo.Client {
+func connectDB() bool {
 
 	clientOptions := options.Client().ApplyURI("mongodb://" + MongoURL + ":" + MongoPort)
 
@@ -23,16 +23,29 @@ func connectDB() *mongo.Client {
 		log.Fatal(err)
 	}
 
-	err = client.Ping(context.TODO(), nil)
+	MONGODBCLIENT = client
+
+	if pingMongoDB() {
+
+		fmt.Println("MongoDB connected at " + MongoURL + ":" + MongoPort)
+		return true
+
+	}
+
+	return false
+
+}
+
+func pingMongoDB() bool {
+
+	err := MONGODBCLIENT.Ping(context.TODO(), nil)
 
 	if err != nil {
 		log.Fatal(err)
+		return false
 	}
 
-	fmt.Println("MongoDB connected at " + MongoURL + ":" + MongoPort)
-
-	CLIENT = client
-	return client
+	return true
 
 }
 
@@ -40,7 +53,7 @@ func findMongoDocument(db string, collec string, filter bson.M) []*bson.D {
 
 	var result []*bson.D
 
-	collection := CLIENT.Database(db).Collection(collec)
+	collection := MONGODBCLIENT.Database(db).Collection(collec)
 
 	cur, err := collection.Find(context.TODO(), filter)
 
@@ -65,7 +78,7 @@ func findMongoDocument(db string, collec string, filter bson.M) []*bson.D {
 
 func insertMongoDocument(db string, collec string, document interface{}) bool {
 
-	collection := CLIENT.Database(db).Collection(collec)
+	collection := MONGODBCLIENT.Database(db).Collection(collec)
 
 	insertResult, err := collection.InsertOne(context.TODO(), document)
 
@@ -80,7 +93,7 @@ func insertMongoDocument(db string, collec string, document interface{}) bool {
 
 func updateMongoDocument(db string, collec string, filter interface{}, update interface{}) [2]int64 {
 
-	collection := CLIENT.Database(db).Collection(collec)
+	collection := MONGODBCLIENT.Database(db).Collection(collec)
 
 	updateResult, err := collection.UpdateOne(context.TODO(), filter, update)
 
@@ -102,7 +115,7 @@ func updateMongoDocument(db string, collec string, filter interface{}, update in
 
 func deleteMongoDocument(db string, collec string, deleteCriteria interface{}) int64 {
 
-	collection := CLIENT.Database(db).Collection(collec)
+	collection := MONGODBCLIENT.Database(db).Collection(collec)
 
 	deleteResult, err := collection.DeleteMany(context.TODO(), deleteCriteria)
 
@@ -116,7 +129,7 @@ func deleteMongoDocument(db string, collec string, deleteCriteria interface{}) i
 
 func disconnectDB() {
 
-	err := CLIENT.Disconnect(context.TODO())
+	err := MONGODBCLIENT.Disconnect(context.TODO())
 
 	if err != nil {
 		log.Fatal(err)
--- core.redis.go
+++ core.redis.go
@@ -11,26 +11,39 @@ var REDISCLIENT *redis.Client
 
 func connectRedis() bool {
 
-	var isRedisNormal bool
-
 	client := redis.NewClient(&redis.Options{
 		Addr:     RedisURL + ":" + RedisPort,
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
 
-	pong, err := client.Ping().Result()
+	REDISCLIENT = client
 
-	if pong == "PONG" && err == nil {
-		REDISCLIENT = client
+	checkRedis := pingRedis()
+
+	if checkRedis {
 		fmt.Println("Redis connected at " + RedisURL + ":" + RedisPort)
-		isRedisNormal = true
 	} else {
-		REDISCLIENT = nil
-		isRedisNormal = false
+		return false
 	}
 
-	return isRedisNormal
+	return checkRedis
+}
+
+func pingRedis() bool {
+
+	var isRedisUp bool
+
+	pong, err := REDISCLIENT.Ping().Result()
+
+	if pong == "PONG" && err == nil {
+		isRedisUp = true
+	} else {
+		isRedisUp = false
+	}
+
+	return isRedisUp
+
 }
 
 func disconnectRedis() {
