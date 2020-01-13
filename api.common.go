6b3f24051230b1f54e600190562c5c0de343592a
Vivek Narang <vivek.narang10@gmail.com>
2020-01-13 17:27:25 -0500

    code refactoring ...

---
+++ api.common.go
@@ -0,0 +1,37 @@
+package main
+
+import "net/http"
+
+func pre(w http.ResponseWriter, r *http.Request) bool {
+
+	if (r.Method == http.MethodPost || r.Method == http.MethodPut) && r.Header.Get("Content-Type") != "application/json" {
+
+		respondWith(w, r, nil, MissingContentType, nil, http.StatusBadRequest)
+		return false
+
+	}
+
+	if r.Header.Get("x-access-token") == "" {
+
+		respondWith(w, r, nil, MissingAccessToken, nil, http.StatusBadRequest)
+		return false
+
+	}
+
+	if areCoreServicesUp() {
+
+		respondWith(w, r, nil, ServiceDownMessage, nil, http.StatusServiceUnavailable)
+		return false
+
+	}
+
+	if !authenticate(r.Header.Get("x-access-token")) {
+
+		respondWith(w, r, nil, InvalidSessionMessage, nil, http.StatusUnauthorized)
+		return false
+
+	}
+
+	return true
+
+}
--- helper.common.go
+++ helper.common.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"net/http"
 	"net/url"
 	"reflect"
 )
@@ -31,39 +30,5 @@ func typeof(value interface{}) string {
 func areCoreServicesUp() bool {
 
 	return pingMongoDB() && pingES() && pingRedis()
-
-}
-
-func pre(w http.ResponseWriter, r *http.Request) bool {
-
-	if (r.Method == http.MethodPost || r.Method == http.MethodPut) && r.Header.Get("Content-Type") != "application/json" {
-
-		respondWith(w, r, nil, MissingContentType, nil, http.StatusBadRequest)
-		return false
-
-	}
-
-	if r.Header.Get("x-access-token") == "" {
-
-		respondWith(w, r, nil, MissingAccessToken, nil, http.StatusBadRequest)
-		return false
-
-	}
-
-	if areCoreServicesUp() {
-
-		respondWith(w, r, nil, ServiceDownMessage, nil, http.StatusServiceUnavailable)
-		return false
-
-	}
-
-	if !authenticate(r.Header.Get("x-access-token")) {
-
-		respondWith(w, r, nil, InvalidSessionMessage, nil, http.StatusUnauthorized)
-		return false
-
-	}
-
-	return true
 
 }
