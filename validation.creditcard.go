b6c12fe1aede3ca14210736bf0f4eaaf1ed61d98
Vivek Narang <vivek.narang10@gmail.com>
2020-01-17 14:54:33 -0500

    Payment option object validation ...

--- validation.creditcard.go
+++ validation.creditcard.go
@@ -8,12 +8,45 @@ func validateCreditCard(w http.ResponseWriter, r *http.Request, creditcard CREDI
 
 	if len(creditcard.Name) == 0 || len(creditcard.Name) > 100 {
 
-		respondWith(w, r, nil, "Credit Card's Name field cannot be empty or greater than 100 characters long", nil, http.StatusBadRequest, false)
+		respondWith(w, r, nil, "Payment options's name field cannot be empty or greater than 100 characters long", nil, http.StatusBadRequest, false)
 		return false
 
 	}
 
-	//TODO: More complex credit card field validations needed here!
+	if len(creditcard.CardNumber) == 0 || len(creditcard.CardNumber) > 16 {
+
+		respondWith(w, r, nil, "Payment options's card number field cannot be empty or greater than 16 characters long", nil, http.StatusBadRequest, false)
+		return false
+
+	}
+
+	if len(creditcard.CardExpiryMM) == 0 || len(creditcard.CardExpiryMM) > 2 {
+
+		respondWith(w, r, nil, "Payment options's expiry month field cannot be empty or greater than 2 characters long", nil, http.StatusBadRequest, false)
+		return false
+
+	}
+
+	if len(creditcard.CardExpiryYY) == 0 || len(creditcard.CardExpiryYY) > 2 {
+
+		respondWith(w, r, nil, "Payment options's expiry year field cannot be empty or greater than 2 characters long", nil, http.StatusBadRequest, false)
+		return false
+
+	}
+
+	if len(creditcard.SecurityCode) == 0 || len(creditcard.SecurityCode) > 3 {
+
+		respondWith(w, r, nil, "Payment options's security code field cannot be empty or greater than 3 characters long", nil, http.StatusBadRequest, false)
+		return false
+
+	}
+
+	if len(creditcard.ZipCode) == 0 || len(creditcard.ZipCode) > 10 {
+
+		respondWith(w, r, nil, "Payment options's zip/pin code field cannot be empty or greater than 10 characters long", nil, http.StatusBadRequest, false)
+		return false
+
+	}
 
 	return true
 
