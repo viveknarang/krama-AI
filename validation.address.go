1a1c73657c8910d86a4b49b0075bf008b3fc74b3
Vivek Narang <vivek.narang10@gmail.com>
2020-01-17 14:38:07 -0500

    Address object validation rules ...

--- validation.address.go
+++ validation.address.go
@@ -20,7 +20,47 @@ func validateAddress(w http.ResponseWriter, r *http.Request, address ADDRESS) bo
 
 	}
 
-	// TODO: More complex validations needed here!
+	if len(address.AddressLineOne) == 0 || len(address.AddressLineOne) > 200 {
+
+		respondWith(w, r, nil, "AddressLineOne field cannot be empty or greater than 200 characters long", nil, http.StatusBadRequest, false)
+		return false
+
+	}
+
+	if len(address.AddressLineTwo) > 200 {
+
+		respondWith(w, r, nil, "AddressLineTwo field cannot be greater than 200 characters long", nil, http.StatusBadRequest, false)
+		return false
+
+	}
+
+	if len(address.City) == 0 || len(address.City) > 100 {
+
+		respondWith(w, r, nil, "Address city field cannot be empty or greater than 100 characters long", nil, http.StatusBadRequest, false)
+		return false
+
+	}
+
+	if len(address.State) == 0 || len(address.State) > 100 {
+
+		respondWith(w, r, nil, "Address state field cannot be empty or greater than 100 characters long", nil, http.StatusBadRequest, false)
+		return false
+
+	}
+
+	if len(address.Country) == 0 || len(address.Country) > 100 {
+
+		respondWith(w, r, nil, "Address country field cannot be empty or greater than 100 characters long", nil, http.StatusBadRequest, false)
+		return false
+
+	}
+
+	if len(address.Pincode) == 0 || len(address.Pincode) > 10 {
+
+		respondWith(w, r, nil, "Address pincode field cannot be empty or greater than 10 characters long", nil, http.StatusBadRequest, false)
+		return false
+
+	}
 
 	return true
 
