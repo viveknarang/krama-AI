b1aee29f6a7d6b644b28d6caaa963267d1e2027a
Vivek Narang <vivek.narang10@gmail.com>
2020-01-21 12:25:09 -0500

    Address validation refactoring ...

--- struct.address.go
+++ struct.address.go
@@ -2,13 +2,13 @@ package main
 
 //ADDRESS document structure
 type ADDRESS struct {
-	FirstName      string `json:"FirstName" bson:"FirstName"`
-	LastName       string `json:"LastName" bson:"LastName"`
-	AddressLineOne string `json:"AddressLineOne" bson:"AddressLineOne"`
-	AddressLineTwo string `json:"AddressLineTwo" bson:"AddressLineTwo"`
-	City           string `json:"City" bson:"City"`
-	State          string `json:"State" bson:"State"`
-	Country        string `json:"Country" bson:"Country"`
-	Pincode        string `json:"Pincode" bson:"Pincode"`
+	FirstName      string `json:"FirstName" bson:"FirstName" validate:"min=1,max=100"`
+	LastName       string `json:"LastName" bson:"LastName" validate:"min=1,max=100"`
+	AddressLineOne string `json:"AddressLineOne" bson:"AddressLineOne" validate:"min=1,max=200"`
+	AddressLineTwo string `json:"AddressLineTwo" bson:"AddressLineTwo" validate:"min=1,max=200"`
+	City           string `json:"City" bson:"City" validate:"min=1,max=100"`
+	State          string `json:"State" bson:"State" validate:"min=1,max=100"`
+	Country        string `json:"Country" bson:"Country" validate:"min=1,max=100"`
+	Pincode        string `json:"Pincode" bson:"Pincode" validate:"min=1,max=10"`
 	Default        bool   `json:"Default" bson:"Default"`
 }
--- validation.address.go
+++ validation.address.go
@@ -2,62 +2,15 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/go-validator/validator"
 )
 
 func validateAddress(w http.ResponseWriter, r *http.Request, address ADDRESS) bool {
 
-	if len(address.FirstName) == 0 || len(address.FirstName) > 100 {
+	if errs := validator.Validate(address); errs != nil {
 
-		respondWith(w, r, nil, "Address FirstName field cannot be empty or greater than 100 characters long", nil, http.StatusBadRequest, false)
-		return false
-
-	}
-
-	if len(address.LastName) == 0 || len(address.LastName) > 100 {
-
-		respondWith(w, r, nil, "Address LastName field cannot be empty or greater than 100 characters long", nil, http.StatusBadRequest, false)
-		return false
-
-	}
-
-	if len(address.AddressLineOne) == 0 || len(address.AddressLineOne) > 200 {
-
-		respondWith(w, r, nil, "AddressLineOne field cannot be empty or greater than 200 characters long", nil, http.StatusBadRequest, false)
-		return false
-
-	}
-
-	if len(address.AddressLineTwo) > 200 {
-
-		respondWith(w, r, nil, "AddressLineTwo field cannot be greater than 200 characters long", nil, http.StatusBadRequest, false)
-		return false
-
-	}
-
-	if len(address.City) == 0 || len(address.City) > 100 {
-
-		respondWith(w, r, nil, "Address city field cannot be empty or greater than 100 characters long", nil, http.StatusBadRequest, false)
-		return false
-
-	}
-
-	if len(address.State) == 0 || len(address.State) > 100 {
-
-		respondWith(w, r, nil, "Address state field cannot be empty or greater than 100 characters long", nil, http.StatusBadRequest, false)
-		return false
-
-	}
-
-	if len(address.Country) == 0 || len(address.Country) > 100 {
-
-		respondWith(w, r, nil, "Address country field cannot be empty or greater than 100 characters long", nil, http.StatusBadRequest, false)
-		return false
-
-	}
-
-	if len(address.Pincode) == 0 || len(address.Pincode) > 10 {
-
-		respondWith(w, r, nil, "Address pincode field cannot be empty or greater than 10 characters long", nil, http.StatusBadRequest, false)
+		respondWith(w, r, nil, "Error(s) found in the address data: "+errs.Error(), nil, http.StatusBadRequest, false)
 		return false
 
 	}
