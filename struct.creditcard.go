16daad6ae6be42eeb95d7f674b4f2d10be9989ec
Vivek Narang <vivek.narang10@gmail.com>
2020-01-21 12:21:07 -0500

    Credit Card validation refactoring ...

--- struct.creditcard.go
+++ struct.creditcard.go
@@ -2,12 +2,12 @@ package main
 
 //CREDITCARD document structure
 type CREDITCARD struct {
-	Name            string `json:"Name" bson:"Name"`
-	CardNumber      string `json:"CardNumber" bson:"CardNumber"`
-	CardExpiryMM    string `json:"CardExpiryMM" bson:"CardExpiryMM"`
-	CardExpiryYY    string `json:"CardExpiryYY" bson:"CardExpiryYY"`
-	SecurityCode    string `json:"SecurityCode" bson:"SecurityCode"`
-	ZipCode         string `json:"ZipCode" bson:"ZipCode"`
+	Name            string `json:"Name" bson:"Name" validate:"min=1,max=100"`
+	CardNumber      string `json:"CardNumber" bson:"CardNumber" validate:"min=16,max=16"`
+	CardExpiryMM    string `json:"CardExpiryMM" bson:"CardExpiryMM" validate:"min=1,max=2"`
+	CardExpiryYY    string `json:"CardExpiryYY" bson:"CardExpiryYY" validate:"min=2,max=2"`
+	SecurityCode    string `json:"SecurityCode" bson:"SecurityCode" validate:"min=3,max=3"`
+	ZipCode         string `json:"ZipCode" bson:"ZipCode" validate:"min=1,max=10"`
 	Default         bool   `json:"Default" bson:"Default"`
 	SaveInformation bool   `json:"SaveInformation" bson:"SaveInformation"`
 }
--- validation.creditcard.go
+++ validation.creditcard.go
@@ -3,27 +3,15 @@ package main
 import (
 	"net/http"
 	"strconv"
+
+	"github.com/go-validator/validator"
 )
 
 func validateCreditCard(w http.ResponseWriter, r *http.Request, creditcard CREDITCARD) bool {
 
-	if len(creditcard.Name) == 0 || len(creditcard.Name) > 100 {
+	if errs := validator.Validate(creditcard); errs != nil {
 
-		respondWith(w, r, nil, "Payment options's name field cannot be empty or greater than 100 characters long", nil, http.StatusBadRequest, false)
-		return false
-
-	}
-
-	if len(creditcard.CardNumber) == 0 || len(creditcard.CardNumber) > 16 {
-
-		respondWith(w, r, nil, "Payment options's card number field cannot be empty or greater than 16 characters long", nil, http.StatusBadRequest, false)
-		return false
-
-	}
-
-	if len(creditcard.CardExpiryMM) == 0 || len(creditcard.CardExpiryMM) > 2 {
-
-		respondWith(w, r, nil, "Payment options's expiry month field cannot be empty or greater than 2 characters long", nil, http.StatusBadRequest, false)
+		respondWith(w, r, nil, "Error(s) found in the credit card data: "+errs.Error(), nil, http.StatusBadRequest, false)
 		return false
 
 	}
@@ -33,27 +21,6 @@ func validateCreditCard(w http.ResponseWriter, r *http.Request, creditcard CREDI
 	if mm < 1 || mm > 12 {
 
 		respondWith(w, r, nil, "Payment options's expiry month field should be between 1 and 12 (inclusive)", nil, http.StatusBadRequest, false)
-		return false
-
-	}
-
-	if len(creditcard.CardExpiryYY) == 0 || len(creditcard.CardExpiryYY) > 2 {
-
-		respondWith(w, r, nil, "Payment options's expiry year field cannot be empty or greater than 2 characters long", nil, http.StatusBadRequest, false)
-		return false
-
-	}
-
-	if len(creditcard.SecurityCode) == 0 || len(creditcard.SecurityCode) > 3 {
-
-		respondWith(w, r, nil, "Payment options's security code field cannot be empty or greater than 3 characters long", nil, http.StatusBadRequest, false)
-		return false
-
-	}
-
-	if len(creditcard.ZipCode) == 0 || len(creditcard.ZipCode) > 10 {
-
-		respondWith(w, r, nil, "Payment options's zip/pin code field cannot be empty or greater than 10 characters long", nil, http.StatusBadRequest, false)
 		return false
 
 	}
