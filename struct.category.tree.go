a5be0ebf892352a74cc37c3882155118c2f95686
Vivek Narang <vivek.narang10@gmail.com>
2020-02-20 15:26:31 -0500

    adding path ...

--- struct.category.tree.go
+++ struct.category.tree.go
@@ -7,5 +7,6 @@ type CATEGORYTREENODE struct {
 	Images     map[string]string `json:"Images" bson:"Images" validate:"min=1,max=100"`
 	Parent     string            `json:"Parent" bson:"Parent" validate:"min=1,max=100"`
 	Children   []string          `json:"Children" bson:"Children" validate:"min=1,max=100"`
+	Path       string            `json:"Path" bson:"Path"`
 	SKUs       []string          `json:"SKUs" bson:"SKUs" validate:"min=1,max=100,hasNoSpaces"`
 }
